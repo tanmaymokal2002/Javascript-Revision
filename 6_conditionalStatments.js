f773745c7e04dde96f71d6415f26f7ed043cd4c4
“Tanmay <tanmaymokal2002@gmail.com>
2023-08-26 21:24:46 +0530

    learned about consitional satatements

---
+++ 6_conditionalStatments.js
@@ -0,0 +1,26 @@
+//if else elseif:
+
+let age = 19;
+if (age === 18) {
+  console.log("You're eligible for license");
+} else if (age > 18) {
+  console.log("You're eligible for license");
+} else {
+  console.log("You're not eligible for license");
+}
+
+//Switc statement:
+
+switch (age) {
+  case age === 18:
+    console.log("You're eligible for license");
+    break;
+  case age > 18:
+    console.log("You're eligible for license");
+    break;
+  case age < 8:
+    console.log("You're eligible for license");
+    break;
+  default:
+    console.log("You're not eligible for license");
+}
