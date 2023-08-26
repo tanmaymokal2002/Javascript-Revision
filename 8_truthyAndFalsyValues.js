0c75d4cb913deb5fabd013084f6889748ed5dd27
“Tanmay <tanmaymokal2002@gmail.com>
2023-08-26 23:28:28 +0530

    learned about tryuthy and falsy values

---
+++ 8_truthyAndFalsyValues.js
@@ -0,0 +1,39 @@
+if (false) {
+  console.log("This won't be executed");
+}
+
+if (0) {
+  console.log("This won't be executed");
+}
+
+if ("") {
+  console.log("This won't be executed");
+}
+
+if (null) {
+  console.log("This won't be executed");
+}
+
+if (undefined) {
+  console.log("This won't be executed");
+}
+
+if (NaN) {
+  console.log("This won't be executed");
+}
+
+if ("Hello") {
+  console.log("This will be executed");
+}
+
+if (42) {
+  console.log("This will be executed");
+}
+
+if ([]) {
+  console.log("This will be executed");
+}
+
+if ({}) {
+  console.log("This will be executed");
+}
