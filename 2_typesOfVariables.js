41f96554e23cb152cb567ccf71432e9c45b32b49
Tanmay Mokal <tanmaymokal@Tanmays-MacBook-Air.local>
2023-08-26 19:29:00 +0530

    learned about variables in javascript

---
+++ 2_typesOfVariables.js
@@ -0,0 +1,22 @@
+//let
+let count = 5;
+if (count > 0) {
+  let message = "Positive";
+  console.log(message);
+}
+
+//const
+const pi = 3.14159;
+// pi = 12;
+console.log(pi);
+
+//var
+function example() {
+  var x = 10;
+  if (true) {
+    var x = 20;
+    console.log(x);
+  }
+  console.log(x);
+}
+example();
