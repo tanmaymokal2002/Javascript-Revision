194b828e283f3d2bf55fd152ba4bee5ce050c8c6
“Tanmay <tanmaymokal2002@gmail.com>
2023-08-26 21:36:32 +0530

    learned about type conversion and coersion

---
+++ 7_conversion_coercion.js
@@ -0,0 +1,11 @@
+//Type Conversion:
+let x = 34;
+console.log(Number(x));
+console.log(String(x));
+console.log(Boolean(x));
+
+//Type coersion:
+let num = 5;
+let numString = "10";
+let result = num + numString; // "510" (num is coerced to a string)
+console.log(result);
