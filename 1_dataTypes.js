5c182554c87cf710c9cf7433126e314b1d07fe2b
Tanmay Mokal <tanmaymokal@Tanmays-MacBook-Air.local>
2023-08-26 17:21:20 +0530

    learned data types in js

---
+++ 1_dataTypes.js
@@ -0,0 +1,15 @@
+let age = 25; // number
+let myName = "Alice"; // string
+let isStudent = true; // boolean
+let myStatus = null; // null
+let city; // undefined
+
+// Symbols are less commonly used in everyday code
+const idSymbol = Symbol("id");
+
+console.log(typeof age); // Output: "number"
+console.log(typeof myName); // Output: "string"
+console.log(typeof isStudent); // Output: "boolean"
+console.log(typeof myStatus); // Output: "object" (null is a special case)
+console.log(typeof city); // Output: "undefined"
+console.log(typeof idSymbol); // Output: "symbol"
