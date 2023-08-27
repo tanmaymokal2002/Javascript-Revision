7cb12665dcfc74756999a213214954ee5d0acbc3
“Tanmay <tanmaymokal2002@gmail.com>
2023-08-27 10:23:52 +0530

    learned about == vs ===

---
+++ 9_===vs===.js
@@ -0,0 +1,7 @@
+//==
+console.log(5 == "5"); // true (string "5" is coerced to number 5)
+console.log(true == 1); // true (boolean true is coerced to number 1)
+
+//===
+console.log(5 === "5"); // false (string "5" is not the same type as number 5)
+console.log(true === 1); // false (boolean true is not the same type as number 1)
